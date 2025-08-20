fa33e87ed7f3a68760576ba251b6a69274a48fec
devwithshams <talktoshamsuddeen.com>
2025-08-21 00:59:54 +0100

    Resolve merge after pulling remote

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.core.paginator import Paginator
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-f
+
 
 
 
