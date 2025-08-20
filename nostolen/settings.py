0c61b5db920254df3aa245bb4e1f559c943c8c85
devwithshams <talktoshamsuddeen.com>
2025-08-21 00:56:03 +0100

    deploy

--- nostolen/settings.py
+++ nostolen/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-isc#uf*w-)^o5rfg5#9%1@g3yv0doj_@*l_@u5a!b$%91^78^1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "yourdomain.com"]
+
 
 
 # Application definition
@@ -119,10 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
+
+
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # where your app’s static files live
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")    # where collectstatic will put them
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+
 
 
 # Default primary key field type
