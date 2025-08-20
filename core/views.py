f320290d28388100ca803ef854a2af2a9afbb906
devwithshams <talktoshamsuddeen.com>
2025-08-21 00:57:45 +0100

    Initial commit ready for deployment

--- core/views.py
+++ core/views.py
@@ -5,9 +5,9 @@ from .models import Report, ImeiCheckLog
 from django.db.models import Count
 from django.db.models import Q
 from django.core.paginator import Paginator
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+f
 
 
 
